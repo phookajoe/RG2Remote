ed8be4dd1254cb5cff5af7f2b39ae5a9824a8876
joseph-devito <devito_joseph@bah.com>
2014-05-07 14:09:24 -0400

    Converted back to HTML added in top right nav links

--- help-nav-include.php
+++ help-nav-include.php
@@ -1,5 +1,14 @@
 <!-- Help Navigation Bar-->
 <div class="container">
-<div></div>
+
+<nav>
+	<ul class="rightHelperNav lato bold right">
+		<li >1-877-447-6667</li>
+		<li>HELP</li>
+		<li>MY TRIPS</li>
+		<li>HI JOHN!</li>
+	</ul>
+	<div class="avatar"></div>
+
 </div>
-			  <!-- /Navigation-->
+<!-- /Navigation-->
